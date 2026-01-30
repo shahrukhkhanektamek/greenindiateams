11d44af5f13093cc93be2856b4edc7fca69b5003
shahrukhsana <sharukhkhanektamek1998@gmail.com>
2026-01-31 01:19:21 +0530

    fasfsa

--- src3/Context/AppContext.js
+++ src3/Context/AppContext.js
@@ -9,7 +9,7 @@ import CustomSidebar from '../components/Provider/CustomSidebar';
 import { reset } from "../navigation/navigationService"; 
 
 export const AppContext = createContext();
-const ENVIRONMENT = "development"; // production or development
+const ENVIRONMENT = "production"; // production or development
 
 
 
